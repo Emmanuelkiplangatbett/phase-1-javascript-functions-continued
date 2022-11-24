c18c6bc55fbc734f7f461856758562403bcc4f94
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-11-24 13:49:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// code  solution here
+function saturdayFun(Fun = "roller-skate"){
+    return`This Saturday, I want to ${Fun}!`
+}
+const mondayWork = function(Work = "go to the office"){
+    return`This Monday, I will ${Work}.`
+}
+function wrapAdjective(flair="*"){
+    return function(happy="special"){
+        return `You are ${flair}${happy}${flair}!`
+    }
+}
